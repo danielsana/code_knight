3471f7fa41ff224c38ed2bf75f405a3364a72509
dansantos2 <danielsana60@gmail.com>
2023-06-20 13:05:12 +0300

    my first commit

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+
+    const [blogs , setBlogs] = useState(); 
+        
+        useEffect(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                   return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                });
+                
+        }, []);
+
+    return (
+        <div className="home">
+            {blogs && <BlogList blogs={blogs} title = "All Blogs!"/>}
+        </div>
+    );
+}
+ 
+export default Home;
